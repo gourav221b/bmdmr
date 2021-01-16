e61ccc010501f59889aa3176fa2e474e730c5f4c
gourav221b <57912157+gourav221b@users.noreply.github.com>
2021-01-16 22:59:46 +0530

    cart functinoning complete

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,7 +1,10 @@
-import React, {useContext} from "react"
+import React, {useContext,useState} from "react"
+import {Link} from 'react-router-dom';
 import {productContext} from "../Global/productContext"
 import {cartContext} from "../Global/cartContext"
-import { Alert } from 'react-alert';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown'
+
 const readmore=()=>
 {
   const bought=document.querySelectorAll('.readMore');
@@ -10,8 +13,10 @@ const readmore=()=>
    {
     var readmore=morecontent.previousElementSibling.firstChild;
     var readless=morecontent.previousElementSibling.lastChild;
+    var prodimage=morecontent.parentElement.parentElement.firstElementChild.firstElementChild;
+   
   
-    readmore.addEventListener('click',()=>
+    prodimage.addEventListener('click',()=>
     {   readmore.style.display="none";
     readless.style.display="flex";
     morecontent.style.display="flex";
@@ -24,22 +29,63 @@ const readmore=()=>
 
     })
    })
+
+  
 }
+
 const Products = () => {
    const {products} = useContext(productContext);
    const {dispatch}= useContext(cartContext);
- 
-   
- 
+  
+   const [value,setValue]=useState(' ');
+  var filterquery="";
+   const [filter,setfilter]=useState('0');
+  const handleSelect=(e)=>{
+    console.log(e);
+    setValue(e);
+   setfilter(1)
+filterquery=`${value}`;
+    
+  }
+  console.log(filterquery)
+  
+
     return(
         <>
+        <div className="filteringDiv">
+        <DropdownButton
+      alignRight
+      title="Filter By: "
+      id="dropdown-menu-align-right"
+      onSelect={handleSelect}
+        >
+           <Dropdown.Item eventKey='' onClick={() => window.location.reload(false)} >All categories</Dropdown.Item>
+              <Dropdown.Item eventKey="Chemicals & Materials">Chemicals & Materials</Dropdown.Item>
+              <Dropdown.Item eventKey="Consumer Goods">Consumer Goods</Dropdown.Item>
+              <Dropdown.Item eventKey="Food & Beverages">Food & Beverages</Dropdown.Item>
+              <Dropdown.Item eventKey="HealthCare">HealthCare</Dropdown.Item>
+              <Dropdown.Item eventKey="ICT">Internet, Communication, Technology</Dropdown.Item>
+              <Dropdown.Item eventKey="Banking, Finance,Insurance">Banking, Finance, Insurance</Dropdown.Item>
+              <Dropdown.Item eventKey="Automotive and Aerospace">Automotive and Aerospace</Dropdown.Item>
+              {/* <Dropdown.Divider />
+              <Dropdown.Item eventKey="some link">some link</Dropdown.Item> */}
+      </DropdownButton>
+              <h4>{filterquery}</h4>
+              {/* <h4>{value}</h4> */}
+        </div>
         
-        <div className="products" style={{marginTop: '30px'}}>
+        
+        {filter !=1 ?  <div className="products" style={{marginTop: '30px'}}>
         {products.map(product => (
             <div className="product row " key={product.id}>
               <div className="pro">
-            <div className="productImage">
-              <img src={product.image} alt="Not found"/>
+            <div className="productImage" onClick={()=>{
+              readmore();
+            }} >
+              <img src={product.image} alt="Not found" className="image"/>
+              <div className="over">
+    <div className="showtext">Click to Read More</div>
+  </div>
             </div>
             <div className="productDetails ">
                 <div className="proName">
@@ -48,9 +94,13 @@ const Products = () => {
                <div className="proPrice">
                    Rs.{product.price}.00
                </div>
-               <div className="proDate">
-                  {product.month<10 ? `0${product.month}` :` ${product.month}`}/{product.year}
+               <div className="row" style={{display:"flex",alignItems:"flex-start",justifyContent:"space-between"}}>
+               <div className="proDate flex col-xs-6">
+              <span style={{color:"gray"}}> Published:</span> {product.month<10 ? `0${product.month}` :` ${product.month}`}/{product.year}
                </div>
+               <div className="proToc flex col-xs-6">
+              <span style={{color:"gray"}}> TOC:</span> {product.toc== true? <i className="fa fa-check-square-o toctrue" aria-hidden="true"></i> :' '}
+               </div></div>
             </div>
             <div className="proButton" onClick={() =>
               {
@@ -70,30 +120,91 @@ const Products = () => {
                {product.productStatus === 'Banking, Finance,Insurance' ? <div className="banking">Banking, Financal Services,Insuarance</div>: ''}
                {product.productStatus === 'Automotive and Aerospace' ? <div className="automotive">Automotive and Aerospace</div>: ''}
             </div>
-            
+            <div className="col-md-6 ">
             <div className="readMore" onClick={()=>{
               readmore();
-            }}><span className="more">Read More</span> <span className="less">Read Less</span></div>
-               <div className="prodDetails row ">
-                 <div className="detailLeft col-md-6">
-                 <p className="descriptions">{product.id}</p>
-                 <p className="format">Format: pptx</p>
-             <p className="cat">Category: {product.productStatus}</p>
-             <a className="samplelink" href={product.Samplelink} download><i className="fa fa-download" aria-hidden="true"></i> Download Sample Document</a>
-             </div>
-             <div className="detailRight col-md-6" style={{marginTop:"0"}}>
-                <h4> TABLE OF CONTENT</h4>
-                <div className="toc">
-              {product.toc}
+            }}><span className="more"> </span> <span className="less">&times;</span></div>
+               <div className="prodDetails  ">
+                 <div className="detailLeft col-md-12">
+                 
+                <h4 style={{background:"#fff",position:"sticky",top:"0"}}> TABLE OF CONTENT</h4>
+                <div className="table">
+              {product.table}
                   </div>
              </div>
                  </div>
 
-        </div>
+        </div></div>
         ))}
         
         </div>
+      :  <div className="products" style={{marginTop: '30px'}}>
+      {products.filter(productAll => 
+  
+  productAll.productStatus == `${value}`).map(product => (
+          <div className="product row " key={product.id}>
+            <div className="pro">
+          <div className="productImage" onClick={()=>{
+            readmore();
+          }} >
+            <img src={product.image} alt="Not found" className="image"/>
+            <div className="over">
+  <div className="showtext">Click to Read More</div>
+</div>
+          </div>
+          <div className="productDetails ">
+              <div className="proName">
+               <h4>{product.name}</h4>
+              </div>
+             <div className="proPrice">
+                 Rs.{product.price}.00
+             </div>
+             <div className="row" style={{display:"flex",alignItems:"flex-start",justifyContent:"space-between"}}>
+             <div className="proDate flex col-xs-6">
+            <span style={{color:"gray"}}> Published:</span> {product.month<10 ? `0${product.month}` :` ${product.month}`}/{product.year}
+             </div>
+             <div className="proToc flex col-xs-6">
+            <span style={{color:"gray"}}> TOC:</span> {product.toc== true? <i className="fa fa-check-square-o toctrue" aria-hidden="true"></i> :' '}
+             </div></div>
+          </div>
+          <div className="proButton" onClick={() =>
+            {
+               dispatch({type:'ADD_TO_CART', id: product.id, products})
+          
+               
+         }
+          }>
+               <button className="buyNow">add to cart</button>
+             </div>
+            
+             {product.productStatus === 'Chemicals & Materials' ? <div className="chemical">Chemicals & Materials</div>: ''}
+             {product.productStatus === 'Consumer Goods' ? <div className="consumer">Consumer Goods</div>: ''}
+             {product.productStatus === 'Food & Beverages' ? <div className="food">Food & Beverages</div>: ''}
+             {product.productStatus === 'HealthCare' ? <div className="healthCare">Healthcare</div>: ''}
+             {product.productStatus === 'ICT' ? <div className="ICT">ICT</div>: ''}
+             {product.productStatus === 'Banking, Finance,Insurance' ? <div className="banking">Banking, Financal Services,Insuarance</div>: ''}
+             {product.productStatus === 'Automotive and Aerospace' ? <div className="automotive">Automotive and Aerospace</div>: ''}
+          </div>
+          <div className="col-md-6 ">
+          <div className="readMore" onClick={()=>{
+            readmore();
+          }}><span className="more"> </span> <span className="less">&times;</span></div>
+             <div className="prodDetails  ">
+               <div className="detailLeft col-md-12">
+               
+              <h4 style={{background:"#fff",position:"sticky",top:"0"}}> TABLE OF CONTENT</h4>
+              <div className="table">
+            {product.table}
+                </div>
+           </div>
+               </div>
+
+      </div></div>
+      ))}
       
+      </div>
+    }
+       
         </>
     )
 
